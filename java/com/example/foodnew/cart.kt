97f13e83ede3d48b7c4fb6dcedd0ed36f29807e4
Osura Samoda <127742266+Osu2002@users.noreply.github.com>
2024-06-27 14:18:14 +0530

    Add files via upload

---
+++ java/com/example/foodnew/cart.kt
@@ -0,0 +1,11 @@
+package com.example.foodnew
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class cart : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_cart)
+    }
+}
---
+++ java/com/example/foodnew/main1.kt
@@ -0,0 +1,13 @@
+package com.example.foodnew
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class main1 : AppCompatActivity() {
+
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main1)
+    }
+}
